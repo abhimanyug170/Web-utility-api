6a40f662fecc6a7fa03fd6fcb4b01346d5a476af
abhimanyug170 <abhimanyug170@gmail.com>
2020-07-02 00:24:33 +0530

    fixed problem in url-shortening functionality

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 from hashlib import sha256
+import random
+import base64
 import json
 
 app = Flask(__name__)
@@ -10,6 +12,7 @@ api = Api(app)
 client = MongoClient('mongodb://localhost:27017')
 db = client['urlDB']
 urlMap = db['urlMap']
+
 
 class getShortUrl(Resource):
     def post(self):
@@ -21,23 +24,21 @@ class getShortUrl(Resource):
         # by hashing
         while(True):
             hashedUrl = sha256(longUrl.encode()).hexdigest()
-            print('hurl', hashedUrl)
-            # convert to base 62
-            base_62 = []
-            if(hashedUrl == 0):
-                shortUrl = 0
-            else:
-                # while(hashedUrl):
-                #     base_62.append(hashedUrl % 62)
-                #     hashedUrl //= 62
-                shortUrl = hashedUrl[:7]
+            shortUrl = base64.b64encode(hashedUrl.encode())[-6:-2].decode().replace('=', '-').replace('/', '_')    # last 1 or 2 characters are always going to be '=' padding
 
-            print('surl', shortUrl)
-            # find if this shortUrl exist
+            # convert to base 62
+            # base_62 = []
+            # if(hashedUrl == 0):
+            #     shortUrl = 0
+            # else:
+            #     # while(hashedUrl):
+            #     #     base_62.append(hashedUrl % 62)
+            #     #     hashedUrl //= 62
+            #     shortUrl = hashedUrl[:7]
+
+            # find if this shortUrl exist and make database entry
             find_result = urlMap.find_one({"_id": shortUrl})
-            print('find_result', find_result)
-            # unique url generated
-            # make database entry
+            
             if(not find_result):    
                 urlMap.insert({
                     "_id": shortUrl,
@@ -53,9 +54,33 @@ class getShortUrl(Resource):
                     'shortUrl': shortUrl,
                     'status': 200
                 })
+            # collision occur (same last digits for 2 url hashes)
+            else:
+                temp_url = longUrl + str(random.randint(0, 10**2))
+                hashedUrl = sha256(temp_url.encode()).hexdigest()
+                shortUrl = base64.b64encode(hashedUrl.encode())[-6:-2].decode().replace('=', '-').replace('/', '_')
+
+                # find if this shortUrl exist and make database entry
+                find_result = urlMap.find_one({"_id": shortUrl})
+                if(not find_result):    
+                    urlMap.insert({
+                        "_id": shortUrl,
+                        "longUrl": longUrl
+                    })
+                    return jsonify({
+                        'shortUrl': shortUrl,
+                        'status': 200
+                    })
+                # if URL calculated previously
+                elif(find_result["longUrl"] == longUrl):
+                    return jsonify({
+                        'shortUrl': shortUrl,
+                        'status': 200
+                    })
+
 
 class getLongUrl(Resource):
-    def get(self):
+    def post(self):
         postedData = request.get_json()
         shortUrl = postedData["shortUrl"]
 
